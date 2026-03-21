955216a18a2e54594d4e05ea82ccedc7c9da9e7a
Michael Swetnam-Burland <mjswetnamburla@wm.edu>
2026-03-21 14:07:25 -0400

    Added basic scene switch

---
+++ src/main.py
@@ -0,0 +1,40 @@
+# Example file showing a circle moving on screen
+import pygame
+from scenes.menu import Menu
+from scenes.end_game import EndGame
+from scene import Scene
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+current_scene: Scene = EndGame()
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    # Draw the scene
+    current_scene.draw(screen)
+    next = current_scene.next_scene()
+    if next is not None:
+        current_scene = next
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
---
+++ src/scene.py
@@ -0,0 +1,9 @@
+from typing import Self
+import pygame
+
+class Scene:
+    def draw(screen: pygame.Surface):
+        pass
+    
+    def next_scene(self) -> Self | None:
+        pass
