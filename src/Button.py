73f65d1cbcf30edf1e3cabf87df2afe263652cab
Prily Jaegle <pmjaegle@wm.edu>
2026-03-21 16:33:41 -0400

    fixed button yea

--- src/Button.py
+++ src/Button.py
@@ -1,33 +1,65 @@
 import pygame, sys
 import pygame.font
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+DEFAULT_TEXT_COLOR = (255, 255, 255)
+DEFAULT_BUTTON_COLOR = (139, 69, 19)
 class Button():
     """Draws the button and checks if clicked """
     isClicked: bool
-    def __init__(self, x: int, y: int, width, height):
-        """Intialze button attributes"""
+    width: int 
+    height: int 
+    x_pos: int # positon
+    y_pos: int
+    button_color: any
+    text_color: any
+    font: str
+
+    
+
+    def __init__(self, x_pos: int, y_pos: int, width: int, height: int, msg: str, text_color = DEFAULT_TEXT_COLOR, button_color = DEFAULT_BUTTON_COLOR):
+        """Intialze button attributes: location on grid, size (width, height), message, textcore, buttoncolor"""
         
         # Dimensions of the button  EX. 200 x 50
         self.width= width
         self.height = height
-        self.x = x 
-        self.y = y 
+        self.x_pos = x_pos
+        self.y_pos = y_pos
 
 
-        self.button_color = (164, 26, 138)
-        self.text_color = (255, 255, 255)
+        self.button_color = button_color
+        self.text_color = text_color
         
         # label the button (e.g, "START")
         self.font = pygame.font.SysFont(None, 48)
-    
 
         # This defines the button's size and clickable area.
-        self.rect = pygame.Rect(x, y, self.width, self.height)
+        self.rect = pygame.Rect(x_pos,  y_pos, self.width, self.height)
 
+        self.rect.center = self.rect.center
+
+       
+       
+
+        self.write_msg(msg)
+
+
+    
+
+
+    def write_msg(self, msg):
+        """Renders and message on the button"""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+  
 
     def draw(self, screen: pygame.Surface, events: list[pygame.event.Event]):
         """ Draws blank button"""
         # Draw blank button and then draw message.
         screen.fill(self.button_color, self.rect)
+        screen.blit(self.msg_image, self.msg_image_rect)
         
         # Check if the event is mousebuttondown then print event
         self.isClicked = False
@@ -43,6 +75,8 @@ class Button():
         
 
 
-
+# import pygame
+# pygame.init()
+# print(pygame.font.get_fonts())
 
 # Draw the bu
--- src/scenes/start_menu.py
+++ src/scenes/start_menu.py
@@ -6,15 +6,12 @@ class StartMenu(Scene):
     testButton: Button
     
     def __init__(self):
-        self.testButton = Button(100, 100, 200,50)
+        self.testButton = Button(540, 335, 200,50, "START")
         pass
 
     def draw(self, screen, events):
         pygame.draw.circle(screen, "red", pygame.Vector2(15, 15), 20)
         self.testButton.draw(screen, events)
-
-        print(self.testButton.isClicked)
-    # if button is clicked go to next secene
 
     
 
